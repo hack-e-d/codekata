3d82fcb8b298fa7df26ff4e006307cbf228edc10
Vijay P V <52239002+hack-e-d@users.noreply.github.com>
2019-06-28 22:26:18 +0530

    Add files via upload

---
+++ Duplicateprint.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=input().split(" ")
+b=[]
+flag=0
+for i in range(0,n):
+    for j in range(i+1,n):
+        if(int(a[i])==int(a[j])):
+            if a[i] not in b:
+                b.append(int(a[i]))
+                flag=1
+b.sort()
+print(*b,end=" ")
+if(flag==0):
+    print("unique")
