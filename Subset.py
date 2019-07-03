45c93fbd50dbde2ecb4c096123e2039cbf005d26
Vijay P V <52239002+hack-e-d@users.noreply.github.com>
2019-07-03 20:47:11 +0530

    Add files via upload

---
+++ Subset.py
@@ -0,0 +1,21 @@
+a,b=input().split(" ")
+a =int(a)
+b =int(b)
+aa=input().split(" ")
+bb=input().split(" ")
+flag=0
+flag1=1
+for i in range(0,b):
+    for j in range(0,a):
+        if(int(bb[i])==int(aa[j])):
+            flag=1
+            print(bb[i])
+            break
+    if(flag==0):
+        flag1=0
+        break
+    flag=0
+if(flag1==1):
+    print("YES")
+else:
+    print("NO")
